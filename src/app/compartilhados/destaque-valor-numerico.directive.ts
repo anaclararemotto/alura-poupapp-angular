3b037502554d650eb8930397d5920110f769f264
Clara <clararemotto@gmail.com>
2025-10-18 10:41:06 -0300

    Reutilizando diretivas

--- src/app/compartilhados/destaque-valor-numerico.directive.ts
+++ src/app/compartilhados/destaque-valor-numerico.directive.ts
@@ -2,7 +2,13 @@
 // Criar diretivas que sejam aplicadas em atributos personalizados
 // como foi feito em uma diretiva, conseguimos reutilizar a logica em outros locais
 
-import { afterRender, Directive, effect, ElementRef, input } from '@angular/core';
+import {
+  afterRender,
+  Directive,
+  effect,
+  ElementRef,
+  input,
+} from '@angular/core';
 
 @Directive({
   selector: '[appDestaqueValorNumerico]',
@@ -10,15 +16,18 @@ import { afterRender, Directive, effect, ElementRef, input } from '@angular/core
 export class DestaqueValorNumericoDirective {
   appDestaqueValorNumerico = input.required<number>();
 
+  corPositiva = input('var(--destaque-receita)');
+  corNegativa = input('var(--destaque-despesa)');
+
   // ElementRef faz referencia a um elemento do DOM
   constructor(elemento: ElementRef<HTMLElement>) {
     // afterRender metodo de ciclo de vida do angular que garante que a interação com o HTML ira acontecer apenas quando for renderizado (Usar sempre que for referenciar um elemento do DOM)
     afterRender(() => {
-        if(this.appDestaqueValorNumerico() > 0){
-      elemento.nativeElement.style.color = 'var(--destaque-receita)';
-        } else if (this.appDestaqueValorNumerico() < 0) {
-            elemento.nativeElement.style.color = 'var(--destaque-despesa)'
-        }
+      if (this.appDestaqueValorNumerico() > 0) {
+        elemento.nativeElement.style.color = this.corPositiva();
+      } else if (this.appDestaqueValorNumerico() < 0) {
+        elemento.nativeElement.style.color = this.corNegativa();
+      } 
     });
 
     // Podemos usar effect no lugar de after render para que toda vez que um input mudar, a mudança ja aconteça
